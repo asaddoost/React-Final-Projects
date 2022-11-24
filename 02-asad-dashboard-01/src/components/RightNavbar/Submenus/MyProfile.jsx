fc2968a2b151cc41a916cc5d4cfde921491230a9
asaddoost <asad.doost@gmail.com>
2022-11-24 15:15:13 +0330

    add second file

---
+++ 02-asad-dashboard-01/src/components/RightNavbar/Submenus/MyProfile.jsx
@@ -0,0 +1,53 @@
+//REACT ROUTER
+import { Link } from "react-router-dom";
+
+//HOOKS
+import useClickOutside from "../../../CustomHooks/ClickOutside";
+import { useState } from "react";
+
+//ICONS , PICS , STYLES
+import styles from "./MyProfile.module.scss";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { ReactComponent as Avatar } from "../../../pics/avatar.svg";
+
+const MyProfile = () => {
+  const [isProfileOpen, setisProfileOpen] = useState(false);
+  let domNode = useClickOutside(() => {
+    setisProfileOpen(false);
+  });
+  return (
+    <div
+      ref={domNode}
+      className={styles.avatar_container}
+      onClick={() => {
+        setisProfileOpen(!isProfileOpen);
+      }}
+    >
+      {/* AVATAR ICON */}
+      <div className={styles.icon_avatar_container}>
+      </div>
+
+      {/* NAME */}
+      <div className={styles.name}>
+        <span>AsadDoost</span>
+        <MdKeyboardArrowDown />
+      </div>
+
+      {/* AVATAR/SETTINGS SUBMENU */}
+      <div
+        className={`${styles.menu} ${isProfileOpen ? styles.menu_active : ""}`}
+      >
+        <div className={styles.info}>
+          <span className={styles.name}>AsadDoos</span>
+          <span className={styles.role}>Admin</span>
+        </div>
+        <div className={styles.settings}>
+          <Link to="/">Settings</Link>
+          <Link to="/">Sign Out</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyProfile;
